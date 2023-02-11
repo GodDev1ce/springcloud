3862770abfca2b3cdde47f88d27f32c368844dd6
Hzl <1040861996@qq.com>
2023-02-11 16:06:18 +0800

    push commit

--- src/main/java/com/lam/Main.java
+++ src/main/java/com/lam/Main.java
@@ -3,5 +3,8 @@ package com.lam;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hellogit1");
+        System.out.println("hellogit1");
+        System.out.println("hellogit1");
+        System.out.println("hellogit1");
     }
 }
